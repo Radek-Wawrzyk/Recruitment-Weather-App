3ba495ce1368b9e92869769f49f5e1aecd82bcfe
Radek <radek511@op.pl>
2018-09-06 19:10:12 +0200

    Add - Header component with styles

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
-    test
+    <Header></Header>
   </div>
 </template>
 
 <script>
 
+import Header from '@/Components/Header.vue'
+
 export default {
   name: 'app',
   components: {
-
+    Header
   }
 }
 
@@ -17,11 +19,20 @@ export default {
 
 <style lang="scss">
 
+//Imports
+
 @import url('https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css');
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin-ext');
 
 * {
   box-sizing: border-box;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+
+//Font settings
+
+html, body, * {
+  font-family: 'Roboto', sans-serif;
 }
 
 </style>
---
+++ src/Components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+    <h1 class="header-title">Recruitment Task</h1>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: 'Header'
+}
+
+</script>
+
+<style lang="scss" scoped>
+  .header {
+    display: flex;
+    align-items: center;
+    height: 60px;
+    padding: 0 25px;
+    background: white;
+    box-shadow: 0 4px 6px -6px rgba(34,34,34,.6);
+    &-title {
+      font-size: 26px;
+      color: #414141;
+    }
+  }
+</style>
